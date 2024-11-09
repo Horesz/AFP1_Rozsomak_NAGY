271e06a35a3f7614b0620b5abaf5c9abfce58b49
Bence63 <bencedeme0610@gmail.com>
2024-11-09 17:31:10 +0100

    Bábu eltávolítása ha másik bábu a helyére lép

--- Projekt/app/src/main/java/com/example/projekt/ChessModel.kt
+++ Projekt/app/src/main/java/com/example/projekt/ChessModel.kt
@@ -13,8 +13,13 @@ class ChessModel {
 
     fun movePiece(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int){
         val movingPiece = pieceAt(fromCol, fromRow) ?: return
+        
+        pieceAt(toCol, toRow).let { piecesBox.remove(it)}
+
         movingPiece.col = toCol
         movingPiece.row = toRow
+
+
     }
 
     fun reset(){

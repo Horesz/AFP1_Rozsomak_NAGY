fea99e9548377b031cc2d61e1e9e140c7f98dd2e
B. Péter <bgtptr@protonmail.com>
2024-11-11 14:58:53 +0100

    Fejlesztés

--- Projekt/app/src/test/java/com/example/projekt/ChessGameTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/ChessGameTest.kt
@@ -4,13 +4,17 @@ import junit.framework.TestCase
 
 class ChessGameTest : TestCase() {
 
-    public override fun setUp() {
-        super.setUp()
+    fun testToString()
+    {
+        assertTrue(ChessGame.toString().contains("0 r n b q k b n r"))
     }
 
     fun testMovePiece() {}
 
     fun testReset() {}
 
-    fun testPieceAt() {}
+    fun testPieceAt() {
+        assertNotNull(ChessGame.pieceAt(0, 0))
+        assertEquals(Player.WHITE, ChessGame.pieceAt(0, 0)?.player)
+    }
 }

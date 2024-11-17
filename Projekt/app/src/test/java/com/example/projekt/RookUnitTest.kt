28f42a66c9b198b945d30ecb0efab9a54cd61624
Bence63 <bencedeme0610@gmail.com>
2024-11-17 19:06:27 +0100

    RookUnitTest befejezése

--- Projekt/app/src/test/java/com/example/projekt/RookUnitTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/RookUnitTest.kt
@@ -1,6 +1,7 @@
 package com.example.projekt
 
 import org.junit.Test
+import org.junit.Assert.*
 
 class RookUnitTest {
         @Test
@@ -9,6 +10,18 @@ class RookUnitTest {
             ChessGame.addPiece(ChessPiece(3, 3, Player.WHITE, ChessMan.ROOK, -1))
             println(ChessGame)
 
+            assertTrue(ChessGame.canMove(Square(3,3), Square(3,0)))
+            assertFalse(ChessGame.canMove(Square(3,3), Square(4,4)))
+            assertTrue(ChessGame.canMove(Square(3,3), Square(7,3)))
+
+            @Test
+            fun canRookMove_blocked() {
+                ChessGame.clear()
+                ChessGame.addPiece(ChessPiece(3,3, Player.WHITE, ChessMan.ROOK, -1))
+                ChessGame.addPiece(ChessPiece(3,3, Player.WHITE, ChessMan.KNIGHT, -1))
+                println(ChessGame)
+                assertFalse(ChessGame.canMove(Square(3,3), Square(7,3)))
+            }
 
         }
     }

df7fe7c99025ee689836308e9186ae4b447329b8
Bence63 <bencedeme0610@gmail.com>
2024-11-17 18:55:44 +0100

    ChessGameTest befejezése

--- Projekt/app/src/test/java/com/example/projekt/ChessGameTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/ChessGameTest.kt
@@ -7,30 +7,41 @@ class ChessGameTest{
 
     @Test
     fun toString_isCorrect() {
+        println(ChessGame)
+        assertTrue(ChessGame.toString().contains("3 . . . . . . . ."))
         assertTrue(ChessGame.toString().contains("0 r n b q k b n r"))
     }
 
     @Test
-    fun testMovePiece() {
-        assertNull(ChessGame.pieceAt(0, 2))
-        ChessGame.movePiece(0, 1, 0, 2)
-        assertNotNull(ChessGame.pieceAt(0, 2))
+    fun movePiece_isCorrect() {
+        assertNull(ChessGame.pieceAt(Square(0,2)))
+        ChessGame.movePiece(Square(0,1), Square(0,2))
+        assertNotNull(ChessGame.pieceAt(Square(0,2)))
     }
 
+
     @Test
-    fun testReset() {
-        assertNull(ChessGame.pieceAt(0, 2))
-        ChessGame.movePiece(0, 1, 0, 2)
-        assertNotNull(ChessGame.pieceAt(0, 2))
+    fun reset_isCorrect() {
+        assertNull(ChessGame.pieceAt(Square(0,2)))
+        ChessGame.movePiece(Square(0,1), Square(0,2))
+        assertNotNull(ChessGame.pieceAt(Square(0,2)))
         ChessGame.reset()
-        assertNull(ChessGame.pieceAt(0, 2))
+        assertNull(ChessGame.pieceAt(Square(0,2)))
     }
 
     @Test
-    fun testPieceAt() {
-        assertNotNull(ChessGame.pieceAt(0, 0))
-        assertEquals(Player.WHITE, ChessGame.pieceAt(0, 0)?.player)
+    fun pieceAt_isCorrect() {
+        assertNotNull(ChessGame.pieceAt(Square(0,0)))
+        assertEquals(Player.WHITE, ChessGame.pieceAt(Square(0,0))?.player)
     }
 
+    @Test
+
+    fun elvisOperator(){
+        val x: Int? = null
+        val y: Int? = 2
+        assertEquals(3, x ?: 3)
+        assertEquals(2, x ?: 3)
+    }
 
 }

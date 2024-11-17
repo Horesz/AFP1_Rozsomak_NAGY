89f0b876dbac83a1e8efb78543ae0ab17f03d2ef
Bence63 <bencedeme0610@gmail.com>
2024-11-17 17:40:13 +0100

    anRookMoveUnitTest class létrehozása

---
+++ Projekt/app/src/test/java/com/example/projekt/CanRookMoveUnitTest.kt
@@ -0,0 +1,14 @@
+package com.example.projekt
+
+import com.example.projekt.ChessGame.addPiece
+import com.example.projekt.ChessGame.clear
+import org.junit.Test
+
+    class CanRookMoveUnitTest {
+        @Test
+        fun canRookMove_singlePiece() {
+            ChessGame.clear()
+            ChessGame.addPiece(ChessPiece(3, 3, Player.WHITE, ChessMan.ROOK, -1))
+            println(ChessGame)
+        }
+    }

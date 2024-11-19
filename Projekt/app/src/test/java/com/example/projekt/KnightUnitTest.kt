4b5d063d5f15d568a8ab65dd15addb87f9d36d6a
B. Péter <bgtptr@protonmail.com>
2024-11-19 18:02:43 +0100

    Adatok változtatása

--- Projekt/app/src/test/java/com/example/projekt/KnightUnitTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/KnightUnitTest.kt
@@ -9,7 +9,7 @@ class KnightUnitTest {
         ChessGame.clear()
         ChessGame.addPiece(ChessPiece(3,3, Player.WHITE, ChessMan.KNIGHT, -1))
         println(ChessGame)
-        assertTrue(ChessGame.canMove(Square(3,3), Square(5,4)))
+        assertTrue(ChessGame.canMove(Square(3,3), Square(2,2)))
         assertTrue(ChessGame.canMove(Square(3,3), Square(4,5)))
     }
 }

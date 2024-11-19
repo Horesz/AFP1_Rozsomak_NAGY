5d329ed7865cbce6ef0627c5adf81f59ab436d6d
B. Péter <bgtptr@protonmail.com>
2024-11-19 18:55:12 +0100

    Frissítés

--- Projekt/app/src/main/java/com/example/projekt/ChessGame.kt
+++ Projekt/app/src/main/java/com/example/projekt/ChessGame.kt
@@ -98,15 +98,14 @@ object ChessGame {
             return false
         }
         val movingPiece = pieceAt(from) ?: return false
-        when(movingPiece.man){
-            ChessMan.KNIGHT -> return canKnightMove(from, to)
-            ChessMan.ROOK -> return canRookMove(from,to)
-            ChessMan.KING -> TODO()
-            ChessMan.QUEEN -> TODO()
-            ChessMan.BISHOP -> return canBishopMove(from, to)
-            ChessMan.PAWN -> TODO()
+        return when(movingPiece.man){
+            ChessMan.KNIGHT -> canKnightMove(from, to)
+            ChessMan.ROOK -> canRookMove(from,to)
+            ChessMan.KING -> canKingMove(from, to)
+            ChessMan.QUEEN -> canQueenMove(from, to)
+            ChessMan.BISHOP -> canBishopMove(from, to)
+            ChessMan.PAWN -> canPawnMove(from, to)
         }
-        return true //FIXME
     }
 
     fun movePiece(from: Square, to: Square){

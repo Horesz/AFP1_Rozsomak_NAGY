30680c28edb4a5fd9abdbc582f6f452d73b0df76
B. Péter <bgtptr@protonmail.com>
2024-11-19 18:59:00 +0100

    AssertFalse beszúrása

--- Projekt/app/src/test/java/com/example/projekt/PawnUnitTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/PawnUnitTest.kt
@@ -10,8 +10,7 @@ class PawnUnitTest {
         println(ChessGame)
         assertTrue(ChessGame.canMove(Square(3, 1), Square(3,2)))
         assertTrue(ChessGame.canMove(Square(3, 1), Square(3,3)))
+
+        assertFalse(ChessGame.canMove(Square(3, 1), Square(4,3)))
     }
-
-
-
 }

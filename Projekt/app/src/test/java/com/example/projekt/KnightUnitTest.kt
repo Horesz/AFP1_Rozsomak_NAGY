24293b631da63236660098b577184f0134ef1080
B. Péter <bgtptr@protonmail.com>
2024-11-19 19:03:49 +0100

    canKnightMove frissítve

--- Projekt/app/src/test/java/com/example/projekt/KnightUnitTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/KnightUnitTest.kt
@@ -10,18 +10,7 @@ class KnightUnitTest {
         ChessGame.addPiece(ChessPiece(3,3, Player.WHITE, ChessMan.KNIGHT, -1))
         println(ChessGame)
 
-        assertTrue(ChessGame.canMove(Square(3,3), Square(2,2)))
-        assertTrue(ChessGame.canMove(Square(3,3), Square(2,3)))
-        assertTrue(ChessGame.canMove(Square(3,3), Square(2,4)))
-
-        assertTrue(ChessGame.canMove(Square(3,3), Square(2,2)))
-        assertTrue(ChessGame.canMove(Square(3,3), Square(2,3)))
-        assertTrue(ChessGame.canMove(Square(3,3), Square(2,4)))
-
-        assertTrue(ChessGame.canMove(Square(3,3), Square(3,2)))
-        assertTrue(ChessGame.canMove(Square(3,3), Square(3,4)))
-
-        assertFalse(ChessGame.canMove(Square(3,3), Square(5,3)))
+        assertFalse(ChessGame.canMove(Square(3,3), Square(5,4)))
 
     }
 }

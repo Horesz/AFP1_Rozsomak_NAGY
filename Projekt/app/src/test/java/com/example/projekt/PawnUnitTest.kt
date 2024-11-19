6cedfbda7211d16a7f24baa73e456b61e5ed84d8
B. Péter <bgtptr@protonmail.com>
2024-11-19 18:57:23 +0100

    AssertTrue beszúrása

--- Projekt/app/src/test/java/com/example/projekt/PawnUnitTest.kt
+++ Projekt/app/src/test/java/com/example/projekt/PawnUnitTest.kt
@@ -9,6 +9,7 @@ class PawnUnitTest {
     fun canPawnMove_firstMove() {
         println(ChessGame)
         assertTrue(ChessGame.canMove(Square(3, 1), Square(3,2)))
+        assertTrue(ChessGame.canMove(Square(3, 1), Square(3,3)))
     }
 
 

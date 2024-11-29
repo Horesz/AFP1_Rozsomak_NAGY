466fa8466cea7b2d4711b6bc844734a9bf1c1cab
Bence63 <bencedeme0610@gmail.com>
2024-11-29 11:30:09 +0100

    SingupActivity kidolgozása

--- Projekt/app/src/main/java/com/example/projekt/SignupActivity.kt
+++ Projekt/app/src/main/java/com/example/projekt/SignupActivity.kt
@@ -1,20 +1,50 @@
 package com.example.projekt
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projekt.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: Databasehelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        databaseHelper = Databasehelper(this)
+
+        binding.signupButton.setOnClickListener { it: View ->
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = binding.signupPassword.text.toString()
+
+            signupDatabase(signupUsername, signupPassword)
+        }
+
+        binding.loginRedirect.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signupDatabase(username: String, password: String){
+        val insertedRowId = databaseHelper.insertUser(username, password)
+        if (insertedRowId != -1L){
+            Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else{
+            Toast.makeText(this, "Signup Failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
